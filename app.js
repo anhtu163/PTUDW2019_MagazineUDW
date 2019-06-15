3594e9ebf3b2fcad69bbe99a7472def71f36b342
ANH TỪ KIM HUỲNH <1612839@student.hcmus.edu.vn>
2019-06-16 00:47:17 +0700

    cập nhật chức năng phân trang, cập nhật lượt xem của bài viết khi người đọc xem bài, cập nhật danh sách bài viết phổ biến sắp xếp theo lượt view, cập nhật chức năng hiển thị giao diện báo lỗi khi người dùng cố tình truy cập đường dẫn lỗi

--- app.js
+++ app.js
@@ -6,10 +6,15 @@ var newsModel = require("./models/news.model");
 var categoryModel = require("./models/category.model");
 
 var app = express();
-
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: true
+}));
 app.engine('hbs',exphbs({
     defaultLayout: 'main.hbs',
-    layoutsDir :'views/_layouts'
+    layoutsDir :'views/_layouts',
+    
 }));
 app.set('view engine','hbs');
 app.use(express.static('public'));
@@ -26,15 +31,19 @@ app.get('/', (req, res) => {
     var p2 = categoryModel.all();
     var p3 = newsModel.top_news_1();
     var p4 = newsModel.top_news_2();
+    var p5 = newsModel.newsByBestViewNum();
+    var p6 = newsModel.newsOrderByViewNum();
 
-    Promise.all([p,p1,p2,p3,p4]).then(([rows,row1s,row2s,row3s,row4s])=>{
+    Promise.all([p,p1,p2,p3,p4,p5,p6]).then(([rows,row1s,row2s,row3s,row4s,row5s,row6s])=>{
         res.render("home", {
 
             news: rows,
             news1: row1s,
             category: row2s,
             top1: row3s,
-            top2: row4s
+            top2: row4s,
+            bestView : row5s,
+            view: row6s
         });
     }).catch(err => {
         console.log(err);
@@ -55,6 +64,26 @@ app.get('/about', (req, res) => {
 
 app.use('/admin/dashboard',require('./routers/admin/admin-router'));
 app.use('/news', require('./routers/news.route'));
+
+/*app.use((req, res, next) => {
+    next(createError(404));
+})
+
+app.use((err, req, res, next) => {
+    var status = err.status || 500;
+    var errorView = 'error';
+    if (status === 404)
+        errorView = '404';
+
+    var msg = err.message;
+    var error = err;
+    res.status(status).render(errorView, {
+        layout: false,
+        msg,
+        error
+    })
+})*/
+
 
 app.listen(3000,()=>{
     console.log('web Server is running at http://localhost:3000');
--- routers/news.route.js
+++ routers/news.route.js
@@ -5,42 +5,76 @@ var categoryModel = require('../models/category.model')
 
 var router = express.Router();
 
-router.get('/:id/post',(req,res)=>{
+router.get('/:id/post',(req,res,next)=>{
     var Id = req.params.id;
-    //console.log(Id);
-    newsModel.detail(Id).then(rows=>{
-        categoryModel.all().then(row2s=>{
-            res.render('image-post', {
-                news: rows,
-                category: row2s
-            });
-        }).catch(err => {
-            console.log(err);
-            res.end('error occured');
+    var p = newsModel.detail(Id);
+    var p1 = categoryModel.all();
+    var p2 = newsModel.addView(Id);
+    var p3 = newsModel.newsByBestViewNum();
+    var p4 = newsModel.newsOrderByViewNum();
+    
+    Promise.all([p,p1,p2,p3,p4]).then(([rows,row1s,row2s,row3s,row4s])=>{
         
-    }).catch(err=>{
-        console.log(err);
-        res.end('error occured');
-    });
-    })
-})
+        res.render('image-post', {
+            news: rows,
+            category: row1s,
+            viewNum :row2s,
+            bestView: row3s,
+            view: row4s
+        });
+    }).catch(next);
+    //console.log(req.body.Title);
+});
 
-router.get('/:id/category', (req, res) => {
+
+
+router.get('/:id/category', (req, res,next) => {
     var Id = req.params.id;
     //console.log(Id);
-    var p1 = newsModel.newsByCat(Id);
+    
     var p2 = categoryModel.catById(Id);
     var p3 = categoryModel.all();
-    Promise.all([p1,p2,p3]).then(([rows,row1s,row2s])=>{
+    
+    var pageNum = req.query.page || 1;
+    if(pageNum < 1){
+        pageNum = 1;
+    }
+    var limit = 1;
+    var offset = (pageNum - 1)*limit;
+
+
+    
+    var p4 = newsModel.pageByCat(Id,limit,offset);
+    var p1 = newsModel.countByCat(Id);
+    
+
+    Promise.all([p1,p2,p3,p4]).then(([rows,row1s,row2s,row3s])=>{
+
+        var pages = [];
+        var total = rows[0].total;
+        var nPages = Math.floor(total/limit);
+        if(total%limit > 0) nPages++;
+
+        
+        for(i=1;i<=nPages;i++)
+        {
+            var active = false;
+            if(+pageNum === i) active = true;
+            var obj = {
+                value: i,
+                active
+            };
+            pages.push(obj);
+        }
+
         res.render('newsByCat', {
-            news: rows,
+            news: row3s,
             category1: row1s,
-            category:row2s
+            category:row2s,
+            pages
+
         });
-    }).catch(err => {
-        console.log(err);
-        res.end('error occured');
-    });
+    }).catch(next);
     /*newsModel.newsByCat(Id)
         .then(rows => {
             categoryModel.catById(Id)
